1a776d0e8a804476fee729b355c684e91292f457
Allie LAIRLAB server <allie.delgiorno@gmail.com>
2017-11-27 15:03:22 -0500

    added ilsvrc-nets code/url, made infer.py work and working on easy_solve at the moment to debug solve and make sure the training works

--- infer.py
+++ infer.py
@@ -3,8 +3,35 @@ from PIL import Image
 
 import caffe
 
+# Allie added:
+import scipy.misc
+import sys
+sys.path.append('./data/pascal') # for the colormap
+import VOClabelcolormap
+
+VOC_COLORMAP = VOClabelcolormap.color_map()
+
+def save_image(image_arr, filename):
+    scipy.misc.toimage(image_arr, cmin=0.0, cmax=255).save(filename)
+
+def convert_2d_class_img_to_rgb_img(class_img):
+    print(class_img.shape)
+    rgb_img = np.empty((class_img.shape[0], class_img.shape[1], 3))
+    for r in range(class_img.shape[0]):
+        for c in range(class_img.shape[1]):
+            try:
+                rgb_img[r, c, :] = VOC_COLORMAP[class_img[r, c]]
+            except:
+                print('problem here:')
+                print(class_img[r,c])
+                print(VOClabelcolormap.color_map(class_img[r, c]).ravel())
+                raise
+    return rgb_img
+
+
 # load image, switch to BGR, subtract mean, and make dims C x H x W for Caffe
-im = Image.open('pascal/VOC2010/JPEGImages/2007_000129.jpg')
+#im = Image.open('pascal/VOC2010/JPEGImages/2007_000129.jpg')
+im = Image.open('pascal/VOC2007/JPEGImages/000166.jpg')
 in_ = np.array(im, dtype=np.float32)
 in_ = in_[:,:,::-1]
 in_ -= np.array((104.00698793,116.66876762,122.67891434))
@@ -17,4 +44,8 @@ net.blobs['data'].reshape(1, *in_.shape)
 net.blobs['data'].data[...] = in_
 # run net and take argmax for prediction
 net.forward()
+out_score = net.blobs['score'].data[0]
 out = net.blobs['score'].data[0].argmax(axis=0)
+
+rgb_out = convert_2d_class_img_to_rgb_img(out)
+save_image(rgb_out, 'filename.png')
--- voc-fcn32s/easy_solve.py
+++ voc-fcn32s/easy_solve.py
@@ -31,7 +31,7 @@ interp_layers = [k for k in solver.net.params.keys() if 'up' in k]
 surgery.interp(solver.net, interp_layers)
 
 # scoring
-val = np.loadtxt('../data/pascal/seg11valid.txt', dtype=str)
+val = np.loadtxt('../data/pascal/easy_VOC2011/seg11valid.txt', dtype=str)
 
 for _ in range(25):
     solver.step(4000)
